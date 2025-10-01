fd0812bec47bcbf9b51f5ee7b1c8838028db7c87
Edmond Chong <140604478+Edmondchong@users.noreply.github.com>
2025-10-01 18:30:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ uploaded_file = st.file_uploader("Upload a Brain MRI image", type=["jpg", "jpeg"
 if uploaded_file is not None:
     # Show uploaded image
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded MRI Image", use_column_width=True)
+    st.image(image, caption="Uploaded MRI Image", use_column_width=150)
 
     if st.button("Predict Tumor Type"):
         # Preprocess
